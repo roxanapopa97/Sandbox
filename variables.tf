b39e79ef91ddc3a5c0470f9092a1146bd03fcf63
Roxana Popa <sanica_97@yahoo.com>
2021-08-26 12:31:45 +0300

    check if kms key is working as expected

--- variables.tf
+++ variables.tf
@@ -45,5 +45,5 @@ variable "cross_region_bucket_name" {
 variable "kms_key_alias_name" {
   description = "Alias name used for KMS key"
   type        = string
-  default     = "encrypted_bucket_kms_key_alias"
+  default     = "encrypted_bucket_kms_key"
 }
