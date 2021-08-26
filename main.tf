b36ef97d4567212bd8ae90e332cff2da6f642540
Roxana Popa <sanica_97@yahoo.com>
2021-08-26 10:33:00 +0300

    check if kms key is working as expected

--- main.tf
+++ main.tf
@@ -12,8 +12,6 @@ resource "aws_kms_key" "mykey" {
 
 resource "aws_s3_bucket" "encryptedbucket" {
   bucket = "${var.s3_bucket_name}-${data.aws_caller_identity.current.account_id}"
-  block_public_acls   = true
-  block_public_policy = true
 
   server_side_encryption_configuration {
     rule {
@@ -32,4 +30,11 @@ resource "aws_s3_bucket" "encryptedbucket" {
     target_bucket = var.s3_logging_bucket_name
     target_prefix = "${var.s3_bucket_name}-${data.aws_caller_identity.current.account_id}"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "access" {
+  bucket = aws_s3_bucket.encryptedbucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
 }
