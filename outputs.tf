57ce321f078f6f4e185adaf54b8f25a4660baea4
Roxana Popa <sanica_97@yahoo.com>
2021-09-12 21:45:14 +0300

    feat(feature-iam): fix count error

--- outputs.tf
+++ outputs.tf
@@ -1,71 +1,71 @@
 # outputs for aws_iam_policy.this
 output "policy_id" {
     description = "The ARN assigned by AWS to this policy"
-    value = aws_iam_policy.this.id
+    value = aws_iam_policy.this[0].id
 }
 
 output "policy_arn" {
     description = "The ARN assigned by AWS to this policy"
-    value = aws_iam_policy.this.arn
+    value = aws_iam_policy.this[0].arn
 }
 
 output "policy_description" {
     description = "The description of the policy"
-    value = aws_iam_policy.this.description
+    value = aws_iam_policy.this[0].description
 }
 
 output "policy_name" {
     description = "The name of the policy"
-    value = aws_iam_policy.this.name
+    value = aws_iam_policy.this[0].name
 }
 
 output "policy_path" {
     description = "The path of the policy in IAM"
-    value = aws_iam_policy.this.path
+    value = aws_iam_policy.this[0].path
 }
 
 output "policy_document" {
     description = "The policy document"
-    value = aws_iam_policy.this.policy
+    value = aws_iam_policy.this[0].policy
 }
 
 output "policy_document_id" {
     description = "The policy's ID"
-    value = aws_iam_policy.this.policy_id
+    value = aws_iam_policy.this[0].policy_id
 }
 
 output "policy_tags" {
     description = "A map of tags assigned to the resource, including those inherited from the provider"
-    value = aws_iam_policy.this.tags_all
+    value = aws_iam_policy.this[0].tags_all
 }
 
 # outputs for aws_iam_role.this
 output "role_arn" {
     description = "Amazon Resource Name (ARN) specifying the role"
-    value = aws_iam_role.this.arn
+    value = aws_iam_role.this[0].arn
 }
 
 output "role_create_date" {
     description = "Creation date of the IAM role"
-    value = aws_iam_role.this.create_date
+    value = aws_iam_role.this[0].create_date
 }
 
 output "role_id" {
     description = "Name of the role"
-    value = aws_iam_role.this.id
+    value = aws_iam_role.this[0].id
 }
 
 output "role_name" {
     description = "Name of the role"
-    value = aws_iam_role.this.name
+    value = aws_iam_role.this[0].name
 }
 
 output "role_tags" {
     description = " A map of tags assigned to the resource, including those inherited from the provider"
-    value = aws_iam_role.this.tags_all
+    value = aws_iam_role.this[0].tags_all
 }
 
 output "role_unique_id" {
     description = "Stable and unique string identifying the role"
-    value = aws_iam_role.this.unique_id
+    value = aws_iam_role.this[0].unique_id
 }
