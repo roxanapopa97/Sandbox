bae1ec1329e1cced6c404c19765f64cb423620ad
Roxana Popa <sanica_97@yahoo.com>
2021-09-12 22:09:35 +0300

    feat(feature-iam): fix outputs errors

--- outputs.tf
+++ outputs.tf
@@ -1,42 +1,42 @@
 # outputs for aws_iam_policy.this
 output "policy_id" {
     description = "The ARN assigned by AWS to this policy"
-    value = aws_iam_policy.this[0].id
+    value = var.create_iam_policy ? aws_iam_policy.this[0].id : null
 }
 
 output "policy_arn" {
     description = "The ARN assigned by AWS to this policy"
-    value = aws_iam_policy.this[0].arn
+    value = var.create_iam_policy ? aws_iam_policy.this[0].arn : null
 }
 
 output "policy_description" {
     description = "The description of the policy"
-    value = aws_iam_policy.this[0].description
+    value = var.create_iam_policy ? aws_iam_policy.this[0].description : null
 }
 
 output "policy_name" {
     description = "The name of the policy"
-    value = aws_iam_policy.this[0].name
+    value = var.create_iam_policy ? aws_iam_policy.this[0].name : null
 }
 
 output "policy_path" {
     description = "The path of the policy in IAM"
-    value = aws_iam_policy.this[0].path
+    value = var.create_iam_policy ? aws_iam_policy.this[0].path : null
 }
 
 output "policy_document" {
     description = "The policy document"
-    value = aws_iam_policy.this[0].policy
+    value = var.create_iam_policy ? aws_iam_policy.this[0].policy : null
 }
 
 output "policy_document_id" {
     description = "The policy's ID"
-    value = aws_iam_policy.this[0].policy_id
+    value = var.create_iam_policy ? aws_iam_policy.this[0].policy_id : null
 }
 
 output "policy_tags" {
     description = "A map of tags assigned to the resource, including those inherited from the provider"
-    value = aws_iam_policy.this[0].tags_all
+    value = var.create_iam_policy ? aws_iam_policy.this[0].tags_all : null
 }
 
 # outputs for aws_iam_role.this
