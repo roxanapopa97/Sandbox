08a7350397c3442b7753381c6efd2af786ac2e4f
Roxana Popa <sanica_97@yahoo.com>
2021-09-17 08:33:35 +0300

    feat(feature-tftest): fix plan path

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ import pytest
 
 @pytest.fixture(scope='session')
 def fixtures_dir():
-  return '/__w/Sandbox/Sandbox/fixtures/plan_output.json'
+  return '/__w/Sandbox/Sandbox/fixtures/'
   #return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixtures')
